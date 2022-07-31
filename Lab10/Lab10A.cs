88bc78603460ac1388260e5c97aee747b7a87759
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2022-07-31 08:43:54 -0400

    Add project files.

---
+++ Lab10/Lab10A.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lab10
+{
+    class Lab10A
+    {
+
+        static void Main(string[] args)
+        {
+            string uI;
+            double x = 0, y = 0, temp = 0, sum = 0;
+
+            Console.Write("enter number 1: ");
+            uI = Console.ReadLine();
+            x = Convert.ToDouble(uI);
+
+            Console.Write("enter number 2: ");
+            uI = Console.ReadLine();
+            y = Convert.ToDouble(uI);
+
+            if (y < x)
+            {
+                temp = x;
+                x = y;
+                y = temp;
+            }
+            
+            Console.WriteLine("Min is " + x);
+
+            Console.WriteLine("Max is " + y);
+
+            sum = amongus(x, y);
+
+
+            Console.WriteLine("Average is " + sum);
+        }
+
+        public static double amongus (double sus1, double sus2)
+        {
+            double sus = 0;
+
+            sus = (sus1 + sus2) / 2;
+
+            return sus;
+        }
+
+    }
+}
---
+++ Lab10/Lab10B.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Lab10
+{
+    class Lab10B
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            string uI;
+            char choice = 'a';
+            double x = 0, y = 0;
+            decimal x1 = 0, y1 = 0;
+            bool isPlaying = true;
+
+            do
+            {
+                Console.Write("enter width: ");
+                uI = Console.ReadLine();
+                x = Convert.ToDouble(uI);
+                x1 = Convert.ToDecimal(x);
+
+                Console.Write("enter height: ");
+                uI = Console.ReadLine();
+                y = Convert.ToDouble(uI);
+                y1 = Convert.ToDecimal(y);
+
+                if (isValid(x,y))
+                {
+                    Console.WriteLine("This is a valid rectangle ");
+
+
+                    Console.WriteLine("The area is: " + area(x,y) + ".0");
+                    Console.WriteLine("The perimeter is: " + perimeter(x,y) + ".0 \n");
+                }
+                else
+                {
+                    Console.WriteLine("This is an invalid rectangle \n");
+                }
+
+                Console.Write("Do you want to enter another width and height (Y/N)? : ");
+                uI = Console.ReadLine();
+                choice = Convert.ToChar(uI);
+                Console.Write("\n");
+
+                isPlaying = (choice == 'Y' | choice == 'y' ? true : false);
+                isPlaying = (choice == 'N' | choice == 'n' ? false : true);
+
+            } while (isPlaying);
+
+            Console.WriteLine("Program Ends ");
+        }
+
+        public static bool isValid(double width, double height)
+        {
+            if ((width + height) > 30)
+                return true;
+            return false;
+        }
+        public static double area(double width, double height)
+        {
+            return (width * height);
+        }
+        public static double perimeter(double width, double height)
+        {
+            return ((width * 2) + (height * 2));
+        }
+
+    }
+}
